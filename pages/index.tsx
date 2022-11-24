b193f79c77ce5e5ec20ece92484bc217218d755f
kamonohashiK <kamonohashiy@gmail.com>
2022-11-24 17:24:29 +0900

    初期のスタイルを削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
-import Head from "next/head";
 import SearchForm from "../components/search_form";
-import Navbar from "../components/navbar";
-import styles from "../styles/Home.module.css";
 import IslandsPerArea from "../components/islands_per_area";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <Navbar />
+    <div>
+      <main>
         <SearchForm />
         <IslandsPerArea />
       </main>
--- pages/island.tsx
+++ pages/island.tsx
@@ -1,11 +1,10 @@
 import AreaGuide from "../components/area_guide";
 import CoverImage from "../components/cover_image";
 import IslandContents from "../components/Island_contents";
-import styles from "../styles/Home.module.css";
 
 function Island() {
   return (
-    <div className={styles.container}>
+    <div>
       <AreaGuide />
       <CoverImage />
       <IslandContents />
